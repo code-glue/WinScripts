26d85440380ca65012fbe5398b80d6c584388600
Ben Lemmond <benlemmond@codeglue.org>
2014-09-06 09:44:27 -0500

    Added tests for DirExists and FileExists

---
+++ Tests/DirExistsTest.bat
@@ -0,0 +1,20 @@
+@echo off
+
+SetLocal
+
+set BatchFile=%~dp0..\DirExists.bat
+
+call "%BatchFile%"
+if %ErrorLevel% neq 1 echo Failed
+
+call "%BatchFile%" C:\Windows
+if %ErrorLevel% neq 0 echo Failed
+
+call "%BatchFile%" C:\Windows\notepad.exe
+if %ErrorLevel% neq 1 echo Failed
+
+call "%BatchFile%" C:\
+if %ErrorLevel% neq 0 echo Failed
+
+call "%BatchFile%" C:\Windows\system32\explorer.exe
+if %ErrorLevel% neq 1 echo Failed
