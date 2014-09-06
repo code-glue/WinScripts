5f8dc6bbbd0cb5843d773f91dd8224852b8cb1a6
Ben Lemmond <benlemmond@codeglue.org>
2014-09-06 15:36:25 -0500

    Bug fixes

--- RestartAdb.bat
+++ RestartAdb.bat
@@ -5,26 +5,28 @@
 SetLocal
 
 set Result=1
+set FileName=%~n0
 set PauseOnError=%~dp0PauseOnError.bat
-
-if "%1" == "/?" goto Usage
+ 
 if not [%1] == [] goto Usage
 
-
 taskkill /im adb.exe /f 2>nul
-if %ErrorLevel% neq 0 goto Exit
+if %ErrorLevel% neq 0 goto ExitResult
 
 adb start-server
 if %ErrorLevel% equ 0 set Result=0
-goto Exit
+goto ExitResult
 
 
 :Usage
 echo. Kills all instances of adb.exe and restarts it.
 echo.
-echo.%FileName% <No Parameters>
+echo.%FileName% ^<No Parameters^>
+goto Exit
+
+:ExitResult
+if %Result% neq 0 call "%PauseOnError%"
 
 
 :Exit
-if %Result% neq 0 call "%PauseOnError%"
 @%ComSpec% /c exit %Result% >nul
