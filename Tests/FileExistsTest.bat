302ab8a3560f65bd3003bb85e49985dea29ed6f6
Ben Lemmond <benlemmond@codeglue.org>
2014-09-06 10:53:28 -0500

    Updated tests

--- Tests/FileExistsTest.bat
+++ Tests/FileExistsTest.bat
@@ -5,16 +5,16 @@ SetLocal
 set BatchFile=%~dp0..\FileExists.bat
 
 call "%BatchFile%"
-if %ErrorLevel% neq 1 echo Failed
+if %ErrorLevel% neq 1 echo Failed [empty]
 
 call "%BatchFile%" C:\Windows\notepad.exe
-if %ErrorLevel% neq 0 echo Failed
+if %ErrorLevel% neq 0 echo Failed C:\Windows\notepad.exe
 
 call "%BatchFile%" C:\Windows
-if %ErrorLevel% neq 1 echo Failed
+if %ErrorLevel% neq 1 echo Failed C:\Windows
 
-call "%BatchFile%" C:\Windows\system32\explorer.exe
-if %ErrorLevel% neq 0 echo Failed
+call "%BatchFile%" C:\Windows\system32\cmd.exe
+if %ErrorLevel% neq 0 echo Failed C:\Windows\system32\cmd.exe
 
 call "%BatchFile%" C:\
-if %ErrorLevel% neq 1 echo Failed
+if %ErrorLevel% neq 1 echo Failed C:\
