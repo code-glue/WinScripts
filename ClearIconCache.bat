a266c5e10255c0bb3054205e16dfa63ab563844d
Ben Lemmond <benlemmond@codeglue.org>
2014-09-06 19:35:46 -0500

    Added ErrorLevel on exit

--- ClearIconCache.bat
+++ ClearIconCache.bat
@@ -5,6 +5,13 @@
 REM This may work only in Vista (SP2?) and up.
 REM It may not even be relevant for Windows 8.
 
+SetLocal
+
+set PauseOnError=%~dp0PauseOnError.bat
+set Result=1
+
 ie4uinit.exe -ClearIconCache
-if ErrorLevel 0 exit /b
-pause
+if %ErrorLevel% equ 0 set Result=0
+
+if %ErrorLevel% neq 0 call "%PauseOnError%"
+@%ComSpec% /c exit %Result% >nul
