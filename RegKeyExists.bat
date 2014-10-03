b5ee64efa3e8c25d662cf34491bbabe292fe7fd0
Ben Lemmond <benlemmond@codeglue.org>
2014-10-03 16:38:55 -0500

    Minor changes

--- RegKeyExists.bat
+++ RegKeyExists.bat
@@ -4,12 +4,11 @@
 
 SetLocal
 
-set ScriptName=%~n0
 set Result=1
 
 if [%1] == [] goto Exit
-if "%~1" == "/?" goto Usage
-if not [%2] == [] goto Usage
+if "%~1" == "/?" call :Usage & goto Exit
+if not [%2] == [] call :Usage & goto Exit
 
 goto TestPath
 
@@ -18,7 +17,7 @@ echo.
 echo Determines whether the given path refers to an existing registry key.
 echo Sets the ErrorLevel variable to 0 if the registry key exists; otherwise, 1.
 echo.
-echo.%ScriptName% KeyName
+echo.%~n0 KeyName
 echo.
 echo.  KeyName  [\\Machine\]FullKey
 echo.           Machine - Name of remote machine, omitting defaults to the
@@ -31,15 +30,15 @@ echo.                          selected ROOTKEY
 echo.
 echo.Examples:
 echo.
-echo.  C:\^>%ScriptName%
+echo.  C:\^>%~n0
 echo.    Sets %%ErrorLevel%% to 1.
 echo.
-echo.  C:\^>%ScriptName% "HKCR"
+echo.  C:\^>%~n0 "HKCR"
 echo.    Sets %%ErrorLevel%% to 0. Registry key exists.
 echo.
-echo.  C:\^>%ScriptName% "HKCU\BadKeyName"
+echo.  C:\^>%~n0 "HKCU\BadKeyName"
 echo.    Sets %%ErrorLevel%% to 1. Registry key does not exist.
-goto Exit
+exit /b
 
 
 :TestPath
