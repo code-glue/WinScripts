822c7ba3d86098de38de5180ad59e6ae085cba16
Ben Lemmond <benlemmond@codeglue.org>
2014-10-03 13:22:00 -0500

    Added support for files without an extension.

--- AddTextFileExtension.bat
+++ AddTextFileExtension.bat
@@ -2,47 +2,89 @@
 
 :: %License%
 
-SetLocal EnableDelayedExpansion
+SetLocal DisableDelayedExpansion
 
 set Result=1
-set PauseOnError=%~dp0PauseOnError.bat
-set ScriptName=%~n0
-set Extension=%~1
-
-if [%1] == [] goto UI
-if "%~1" == "/?" goto Usage
-if not [%2] == [] goto Usage
-
-goto GetExtension
-
-
-:UI
-call :PrintHeader
+goto BeginScript
 
 
 :EnterExtension
-set /p Extension="Enter file extension [Ctrl+C to exit]: " %=%
-if %ErrorLevel% neq 0 set "Extension=" & verify >nul & goto EnterExtension
+call :PrintHeader
+:EnterExtensionSub
+set /p "Extension=Enter file extension [Ctrl+C to exit]:"
+SetLocal EnableDelayedExpansion
+if .!Extension! == . EndLocal & goto EnterExtensionSub
+set "NoSpaces=!Extension: =!"
+if .!NoSpaces! == . EndLocal & goto EnterExtensionSub
+EndLocal
+call :Validate
+if %ErrorLevel% neq 0 set Result=2 & goto ExitResult
+goto DoWork
 
-if "%Extension%" == "" goto EnterExtension
-if "%Extension%" == "." goto EnterExtension
-if "%Extension%" == ".." goto EnterExtension
+
+:BeginScript
+set Extension=%1
+SetLocal EnableDelayedExpansion
+if .!Extension! == . EndLocal & goto EnterExtension
+if !Extension! == /? EndLocal & call :Usage & goto Exit
+set Arg2=%2
+if not .!Arg2! == . EndLocal & call :Usage & goto Exit
+EndLocal
+set "Extension=%~1"
 
 
-:GetExtension
-set Extension=.!Extension!
-for %%a in ("!Extension!") do (
-    call set Extension=%%~xa
-)
-
-reg add "HKCR\!Extension!" /v "PerceivedType" /d "text" /f >nul
-if !ErrorLevel! equ 0 set Result=0
-
-reg add "HKCR\!Extension!\PersistentHandler" /ve /d "{5E941D80-BF96-11CD-B579-08002B30BFEB}" /f > nul
-if !ErrorLevel! neq 0 set Result=1
-
-if !Result! equ 0 echo Added plain text persistent handler for file extension: !Extension!
+:DoWork
+SetLocal EnableDelayedExpansion
+EndLocal
+call :Validate
+if %ErrorLevel% neq 0 set Result=2 & goto ExitResult
+call :CleanExtension
+if %ErrorLevel% neq 0 set Result=2 & goto ExitResult
+call :UpdateRegistry
+if %ErrorLevel% equ 0 set Result=0
 goto ExitResult
+
+
+:Validate
+SetLocal EnableDelayedExpansion
+set NoSymbols=!Extension!
+set "NoSpaces=!NoSymbols: =!"
+if .!NoSpaces! == . exit /b 1
+
+set "NoSymbols=!NoSymbols:/=!"
+set "NoSymbols=!NoSymbols:\=!"
+set "NoSymbols=!NoSymbols::=!"
+set "NoSymbols=!NoSymbols:?=!"
+set "NoSymbols=!NoSymbols:<=!"
+set "NoSymbols=!NoSymbols:>=!"
+set "NoSymbols=!NoSymbols:|=!"
+set "NoSymbols=!NoSymbols:"=!"
+set "NoSymbols=!NoSymbols:**=!"
+set "NoSymbols=!NoSymbols:"=!"
+
+if not !Extension! == !NoSymbols! exit /b 1
+exit /b 0
+
+
+:CleanExtension
+if not "%Extension%" == "." (
+    for /f "tokens=*" %%a in (".%Extension%") do (
+        SetLocal EnableDelayedExpansion
+        set Ext=%%~xa
+        if .!Ext! == . exit /b 1
+        EndLocal
+        set Extension=%%~xa
+    )
+)
+exit /b 0
+
+
+:UpdateRegistry
+reg add "HKCR\%Extension%" /v "PerceivedType" /d "text" /f >nul
+if %ErrorLevel% neq 0 exit /b 1
+reg add "HKCR\%Extension%\PersistentHandler" /ve /d "{5E941D80-BF96-11CD-B579-08002B30BFEB}" /f > nul
+if %ErrorLevel% neq 0 exit /b 1
+exit /b 0
 
 
 :PrintHeader
@@ -54,26 +96,43 @@ exit /b 0
 
 :Usage
 call :PrintHeader
-echo.%ScriptName% [.]Extension
+echo.%~n0 [[.]Extension]
 echo.
 echo.  Extension    The name of the extension to add, optionally prefixed by ".".
+echo.               Use "." for files without an extension.
+echo.               Omit to prompt for the extension.
 echo.
 echo.Examples:
 echo.
-echo.  C:\^>%ScriptName%
+echo.  C:\^>%~n0
 echo.    Prompts for the file extension.
 echo.
-echo.  C:\^>%ScriptName% "txt"
+echo.  C:\^>%~n0 "txt"
 echo.    Adds a plain text handler for the .txt file extension.
 echo.
-echo.  C:\^>%ScriptName% ".txt"
+echo.  C:\^>%~n0 ".txt"
 echo.    Adds a plain text handler for the .txt file extension.
-goto Exit
+echo.
+echo.  C:\^>%~n0 .
+echo.    Adds a plain text handler for files without an extension.
+exit /b
 
 
 :ExitResult
-if %Result% neq 0 call "%PauseOnError%"
+SetLocal EnableDelayedExpansion
+if !Result! equ 0 (
+    echo Added plain text persistent handler for file extension: !Extension!
+) else (
+    if !Result! equ 1 (
+        echo Failed to update extension: !Extension!
+    ) else (
+        echo Invalid extension: !Extension!
+    )
+)
+EndLocal
+
 
 
 :Exit
+call "%~dp0PauseIfGui.bat" "%~f0"
 @%ComSpec% /c exit %Result% >nul
