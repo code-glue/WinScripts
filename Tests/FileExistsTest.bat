afc5d95279fcb51a9de4180ea3aea068b852d3bb
Ben Lemmond <benlemmond@codeglue.org>
2014-10-03 16:38:32 -0500

    Updated unit tests

--- Tests/FileExistsTest.bat
+++ Tests/FileExistsTest.bat
@@ -4,17 +4,44 @@ SetLocal
 
 set BatchFile=%~dp0..\FileExists.bat
 
+set Desc=[empty]
 call "%BatchFile%"
-if %ErrorLevel% neq 1 echo Failed [empty]
+if %ErrorLevel% neq 1 goto ExitFail
 
+set Desc=Valid file: C:\Windows\notepad.exe
 call "%BatchFile%" C:\Windows\notepad.exe
-if %ErrorLevel% neq 0 echo Failed C:\Windows\notepad.exe
+if %ErrorLevel% neq 0 goto ExitFail
 
+set Desc=Valid directory: C:\Windows
 call "%BatchFile%" C:\Windows
-if %ErrorLevel% neq 1 echo Failed C:\Windows
+if %ErrorLevel% neq 1 goto ExitFail
 
+set Desc=Valid file: C:\Windows\system32\cmd.exe
 call "%BatchFile%" C:\Windows\system32\cmd.exe
-if %ErrorLevel% neq 0 echo Failed C:\Windows\system32\cmd.exe
+if %ErrorLevel% neq 0 goto ExitFail
 
+set Desc=Valid drive: C:\
 call "%BatchFile%" C:\
-if %ErrorLevel% neq 1 echo Failed C:\
+if %ErrorLevel% neq 1 goto ExitFail
+
+set Desc=Valid file: C:\Windows\system32\explorer.exe
+call "%BatchFile%" C:\Windows\system32\explorer.exe
+if %ErrorLevel% neq 0 goto ExitFail
+
+set Desc=Missing file: C:\Windows\notepad.exeXXXXXX
+call "%BatchFile%" C:\Windows\notepad.exeXXXXXX
+if %ErrorLevel% neq 1 goto ExitFail
+
+
+echo.
+echo All tests passed
+goto Exit
+
+
+:ExitFail
+echo.
+echo Test failed: %Desc%
+
+
+:Exit
+call "%~dp0..\PauseIfGui.bat" "%~f0"
--- Tests/RegKeyExistsTest.bat
+++ Tests/RegKeyExistsTest.bat
@@ -4,14 +4,32 @@ SetLocal
 
 set BatchFile=%~dp0..\RegKeyExists.bat
 
+set Desc=[empty]
 call "%BatchFile%"
-if %ErrorLevel% neq 1 echo Failed [empty]
+if %ErrorLevel% neq 1 goto ExitFail
 
+set Desc=HKCR
 call "%BatchFile%" HKCR
-if %ErrorLevel% neq 0 echo Failed HKCR
+if %ErrorLevel% neq 0 goto ExitFail
 
+set Desc=HKCR\BadPath
 call "%BatchFile%" HKCR\BadPath
-if %ErrorLevel% neq 1 echo Failed HKCR\BadPath
+if %ErrorLevel% neq 1 goto ExitFail
 
+set Desc=HKCR\.txt
 call "%BatchFile%" HKCR\.txt
-if %ErrorLevel% neq 0 echo Failed HKCR\.txt
+if %ErrorLevel% neq 0 goto ExitFail
+
+
+echo.
+echo All tests passed
+goto Exit
+
+
+:ExitFail
+echo.
+echo Test failed: %Desc%
+
+
+:Exit
+call "%~dp0..\PauseIfGui.bat" "%~f0"
